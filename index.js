be2d3f4a095275a68f8364ef433864771b379459
Nikhil Kumar <nikhilkumarsingh166@gmail.com>
2022-11-02 18:03:20 +0530

    changed type of input

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
 const billAmount = document.querySelector('#bill-amount');
-const cashGiven = document.querySelector('#cash-given');
+const cashGiven = document.querySelector('#cash_given');
 const checkButton = document.querySelector('#check-button');
 const message = document.querySelector('#error-message');
 const noOfNotes = document.querySelectorAll('.no-of-notes');
 
 const avilableNotes = [2000,500,100,20,10,5,1];
 
+
+
+
 checkButton.addEventListener('click', function validateBillAmount(){
 
-    if(billAmount.value > 0){
-        if(cashGiven.value >=  billAmount.value)
+    if(billAmount.value > 0)
+    {
+        // console.log(billAmount.value, cashGiven.value);
+        // console.log(typeof(billAmount.value), typeof(cashGiven.value));
+        if( Number(cashGiven.value) < Number(billAmount.value))
         {
-            const amountToBeReturned = cashGiven.value - billAmount.value;
-            calculateChange(amountToBeReturned);
+            showMessage("Do you want to wash plates ? ");
         }
         else{
-            showMessage("Do you want to wash plates ?")
+            const amountToBeReturned = cashGiven.value-billAmount.value;
+            calculateChange(amountToBeReturned);
         }
 
     }else{
@@ -34,6 +40,7 @@ function calculateChange(amountToBeReturned){
         noOfNotes[i].innerText = numberOfNotes;
     }
 }
+
 
 function hideMessage(){
     message.style.display = "none";    
